69286d94e0cbc1e1dc14a4b5d121f85c108b9ad7
Khalotis11 <moekhaloti@gmail.com>
2019-05-23 01:01:34 +0000

    Added Nav Bar Components

---
+++ droom/src/components/About.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const About = () => {
+  return <h5>About</h5>;
+};
---
+++ droom/src/components/SignUp.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SignUp = () => {
+  return <h5>Sign Up</h5>;
+};
